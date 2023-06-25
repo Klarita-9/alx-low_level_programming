7b8aa59bc2d0817b36f16c63ffd62639e5e5cdd6
Claire Wamaitha Waithaka <clairewaithaka34@gmail.com>
2023-06-25 14:59:34 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+write(2, "and that piece of art is useful\" - Dora Korpar, i2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, i2015-10-19"));
 return (1);
 }
 
