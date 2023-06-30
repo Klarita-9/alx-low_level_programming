f385a5e4ce6b34e7463502caeb94afbc273edb83
Claire Wamaitha Waithaka <clairewaithaka34@gmail.com>
2023-06-29 18:07:33 -0700

    update

---
+++ 0x06-pointers_arrays_strings/3-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char s1[] = "Hello";
+char s2[] = "World!";
+
+printf($ (char *s1, s2));
+printf($ char *s2, s1));
+printf("%d\n", _strcat(s1, s1));
+return (0);
+}
+
