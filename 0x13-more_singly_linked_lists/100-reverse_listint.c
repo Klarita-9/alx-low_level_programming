ea9fcf899d0f6772a86e478e89407c0651d58973
Claire Wamaitha Waithaka <clairewaithaka34@gmail.com>
2023-07-31 15:21:28 -0700

    my file

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,21 +8,19 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *p;
-	listint_t *n;
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
 
-	p = NULL;
-	n = NULL;
-
-	while (*head != NULL)
+	while (*head)
 	{
-		n = (*head) ->next;
-		(*head)->next = p;
-		p = *head;
-		*head = n;
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
 	}
 
-	*head = p;
+	*head = prev;
+
 	return (*head);
 }
 
