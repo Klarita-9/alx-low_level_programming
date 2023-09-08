260f9934e65b9e9e2bd07916f2c4ec5e44b3fbc8
Claire Wamaitha Waithaka <clairewaithaka34@gmail.com>
2023-09-08 12:35:06 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * read_textfile - reads a text file and prints the letters
  * @filename: the name of the file
@@ -10,28 +9,30 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t nrd, nwr;
-	char *buf;
+	int fd_read, count_chars, fd_open;
+	char *buf_letters;
 
-	if (!filename)
+	if (!filename == NULL)
 		return (0);
-
-	fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
+	fd_open = open(filename, O_RDONLY);
+	if (fd_open == -1)
 		return (0);
-
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
+	buf_letters = malloc(sizeof(char) * letters);
+	if (buf_letters == NULL))
 		return (0);
-
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
-
-	close(fd);
-
-	free(buf);
-
-	return (nwr);
+	fd_read = read(fd_open, buf_letters, letters);
+	if (fd_read == -1)
+	{
+		free(buf_letters);
+		return (0);
+	}
+	count_chars = write(STDOUT_FILENO, buf_letters, fd_read);
+	if (count_chars == -1)
+	{
+		free(buf_letters);
+		return (0);
+	}
+	close(fd_open);
+	free(buf_letters);
+	return (count_chars);
 }
