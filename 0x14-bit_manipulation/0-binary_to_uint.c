a405a72825851533178191d6af62287a80172866
Claire Wamaitha Waithaka <clairewaithaka34@gmail.com>
2023-09-14 02:18:51 -0700

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "<stddef.h>"
 
 /**
  * binary_to_uint - converts a binary number to an
@@ -10,27 +9,29 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
-	int i;
+	unsigned int ui;
+	int len, base_two;
 	
-	/* if string is empty return 0 */
-	if (b == NULL)
+	if (!b)
 		return (0);
 
-	/* return if the value is different to 0 or 1*/
-	for (i = 0; b[i] != '\0'; i++)
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
+	ui = 0;
 
-	/* converter array the number binary to int */
-	for (i = 0; b[i] != '\0'; i++)
+	for (len = 0; b[len] != '\0'; len++)
+		;
+	
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		num *= 2;
-		if (b[i] == '1')
-			num += 1;
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
-	return (num);
 
-
-
+	return (ui);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,13 +1,6 @@
 #define main_h
 #ifndef main_h
 
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-
-
-
-/*Functions*/
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -15,10 +8,6 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
-
-/*Helper functions*/
-void print_num_binary(unsigned long int n);
 int _putchar(char c);
-
 
 #endif
